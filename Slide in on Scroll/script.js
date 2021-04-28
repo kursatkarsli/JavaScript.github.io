6b20b02e3c1c3024d6f7f9490ff5a54511d2a3e0
Kürşat Karslı <61808010+kursatkarsli@users.noreply.github.com>
2021-04-28 08:11:14 +0300

    Slide in at Scroll

---
+++ Slide in on Scroll/script.js
@@ -0,0 +1,34 @@
+ function debounce(func, wait = 20, immediate = true) {
+     let timeout;
+     return function() {
+          let context = this, args = arguments;
+          let later = function () {
+              timeout = null;
+              if (!immediate) func.apply(context, args);
+          };
+          let callNow = immediate && !timeout;
+          clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
+
+    const sliderImages = document.querySelectorAll('.slide-in');
+
+    function checkSlide() {
+      sliderImages.forEach(sliderImage => {
+        // half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+          sliderImage.classList.add('active');
+        } else {
+          sliderImage.classList.remove('active');
+        }
+      });
+    }
+
+    window.addEventListener('scroll', debounce(checkSlide));
